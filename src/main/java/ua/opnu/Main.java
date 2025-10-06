bbbc62a8bffc92d1e28b5b75f54494e3edf02e98
angelikst <9334002@stud.op.edu.ua>
2025-10-06 23:17:22 +0300

    lab 1 finished

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -15,7 +15,6 @@ public class Main {
      * icyHot(2, 120) → false
      */
     public boolean icyHot(int temp1, int temp2) {
-
         if ((temp1 > 100 && temp2 < 0) || (temp1 < 0 && temp2 > 100)) {
             return true;
         } else {
